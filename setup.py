887b60529b308d6ecb328fcda48e409788791757
Martin Aryee <martin.aryee@gmail.com>
2016-03-21 15:14:32 -0400

    Added cookiecutter-python-cli skeleton

---
+++ setup.py
@@ -0,0 +1,48 @@
+"""
+A preprocessing and QC pipeline for ChIA-PET data.
+"""
+from setuptools import find_packages, setup
+
+dependencies = ['click']
+
+setup(
+    name='dnaloop',
+    version='0.1.0',
+    url='https://github.com/martinaryee/dnaloop',
+    license='BSD',
+    author='Caleb Lareau and Martin Aryee',
+    author_email='aryee.martin@mgh.harvard.edu',
+    description='A preprocessing and QC pipeline for ChIA-PET data.',
+    long_description=__doc__,
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=dependencies,
+    entry_points={
+        'console_scripts': [
+            'preprocess_chiapet = dnaloop.cli:main',
+        ],
+    },
+    classifiers=[
+        # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        # 'Development Status :: 1 - Planning',
+        # 'Development Status :: 2 - Pre-Alpha',
+        # 'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
+        # 'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 6 - Mature',
+        # 'Development Status :: 7 - Inactive',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Operating System :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
