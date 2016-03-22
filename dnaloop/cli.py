34aa2ec32a5a838f5c1864f4da09ad795859b551
Martin Aryee <martin.aryee@gmail.com>
2016-03-22 00:43:02 -0400

    First pipeline commit

--- dnaloop/cli.py
+++ dnaloop/cli.py
@@ -1,10 +1,55 @@
 import click
+import os
+from subprocess import call
 
+def parse_manifest(manifest):
+    samples = []
+    with open(manifest) as f:
+        lines = f.readlines()
+        for line in lines:
+            fields = line.strip().split("\t")
+            if len(fields)==3:
+                d = {   'name': fields[0], 
+                        'read1': fields[1],
+                        'read2': fields[2]
+                        }
+                samples.append(d)
+            else:
+                if line != "\n":
+                    click.echo ("Skipping line: " + line)
+    return samples
 
 @click.command()
-@click.option('--as-cowboy', '-c', is_flag=True, help='Greet as a cowboy.')
-@click.argument('name', default='world', required=False)
-def main(name, as_cowboy):
+#@click.option('--cluster-command', '-c', default='bsub', help='Cluster submit command')
+@click.option('--out', default=".", required=True, help='Output folder name')
+@click.option('--bwa-index', required=True, help='BWA Index')
+@click.argument('manifest')
+#def main(manifest, cluster):
+def main(manifest, out, bwa_index):
     """A preprocessing and QC pipeline for ChIA-PET data."""
-    greet = 'Howdy' if as_cowboy else 'Hello'
-    click.echo('{0}, {1}!'.format(greet, name))
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    out = os.path.abspath(out)    
+    bwa_index = os.path.abspath(bwa_index)    
+    click.echo("Output folder: %s" % out) 
+    click.echo("BWA index: %s\n" % bwa_index)     
+    # Preprocess individual samples
+    samples = parse_manifest("samples.txt")
+    i = 0
+    for sample in samples:
+        i += 1
+        click.echo("Processing sample %d of %d: %s" % (i, len(samples), sample['name']))
+        click.echo("    Read 1: %s" % sample['read1']) 
+        click.echo("    Read 2: %s" % sample['read2'])    
+        preproc_fastq = os.path.join(script_dir, 'preprocess_chiapet_fastq.sh')
+        cmd = [preproc_fastq, os.path.join(out, 'samples', sample['name']), bwa_index, sample['read1'], sample['read2']]
+        click.echo("    Executing: %s\n" % " ".join(cmd))
+        call(cmd)
+        click.echo("\n#################################################\n")
+    # Create the ChIA-PET analysis set
+    preproc_set = os.path.join(script_dir, 'preprocess_chiapet_set.sh')
+    cmd = [preproc_set, out] + [os.path.join(out, 'samples', x['name']) for x in samples]
+    click.echo("Creating ChIA-PET set")
+    click.echo("    Executing: %s\n" % " ".join(cmd))
+    call(cmd)
+
+
