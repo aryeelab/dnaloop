a4e49eb589c2dc7698ccf2ada5585324c74d778c
Martin Aryee <martin.aryee@gmail.com>
2016-03-31 22:27:48 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ dependencies = ['click', 'pyyaml', 'cutadapt', 'MACS2', 'pandas', 'matplotlib']
 
 setup(
     name='dnaloop',
-    version='0.5.5',
+    version='0.5.6',
     url='https://github.com/aryeelab/dnaloop',
     license='BSD',
     author='Caleb Lareau and Martin Aryee',
