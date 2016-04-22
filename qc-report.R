cd3c863f8c9982242df10d6c048db3d1c7ac6cbe
Martin Aryee <martin.aryee@gmail.com>
2016-04-21 23:06:13 -0400

    Added command line args to qc-report.R

--- qc-report.R
+++ qc-report.R
@@ -1,3 +1,9 @@
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
+if (length(args)==0) {
+    stop("ChIA-PET set directory must be specified.\n", call.=FALSE)
+}
+
 library(ggplot2)
 library(scales)
 library(foreach)
@@ -5,14 +11,24 @@ library(gridExtra)
 library(reshape2)
 library(dplyr)
 
-dir <- "/PHShome/ma695/work/projects/chiapet_design/output/dnaloop-0.5.14/esc_010/"
+#dir <- "/PHShome/ma695/work/projects/chiapet_design/output/dnaloop-0.5.14/esc_010/"
+dir <- args[1]
+
+if (length(args)==2) {
+    pdf <- args[2]
+} else {
+    pdf <- file.path(dir, "qc-report.pdf")    
+}
+
+message("Processing: ", dir)
+message("Saving QC report to: ", pdf)
 
 samples <- dir(dir, pattern = ".loop_counts.bedpe")
 samples <- sub(".loop_counts.bedpe", "", samples)
 
 # Creates a dataframe of summary statistics from the individual sample log output files
 readstats <- foreach(sample = samples, .combine="rbind") %do% {
-    sfilename <- paste(dir, "log/", sample, ".read_stats.txt", sep="")
+    sfilename <- file.path(dir, paste("log/", sample, ".read_stats.txt", sep=""))
     rs <- read.table(sfilename, header=FALSE, stringsAsFactors = FALSE)
     rs <- cbind(sample=sample, rs)
     colnames(rs) <- c("sample", "metric", "count")
@@ -23,7 +39,7 @@ readstats <- subset(readstats, metric %in% metrics)
 
 # Get loop lengths and counts
 loop_pets <- foreach(sample = samples, .combine="rbind") %do% {
-    sfilename <- paste(dir, sample , ".loop_counts.bedpe",sep="")
+    sfilename <- file.path(dir, paste(sample , ".loop_counts.bedpe",sep=""))
     x <- read.table(sfilename, stringsAsFactors = FALSE)
     intra <- x[,1]==x[,4]
     x <- x[intra,]
@@ -52,7 +68,7 @@ p_metrics <- p + scale_y_continuous(labels=comma)
 p_hist <- ggplot(loop_pets, aes(loop_length)) + geom_histogram() + scale_x_log10(labels=comma, breaks=10^(3:9)) + facet_wrap(~sample, ncol=1) + theme_bw()
 
 # Output graphics and table to PDF file
-pdf("qc-Report.pdf", height=6, width=11, onefile = TRUE)
+pdf(pdf, height=6, width=11, onefile = TRUE)
 p_metrics
 plot.new()
 grid.table(format(acast(readstats, metric~sample, sum), big.mark=","))
